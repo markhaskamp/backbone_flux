3889b39b83534e8c6474258ca014cc1a5998ac1c
Mark Haskamp <mark.haskamp@pinnsg.com>
2014-08-31 12:05:03 -0400

    painting 'eddie' from its backbone view

--- app/index.js
+++ app/index.js
@@ -1,5 +1,12 @@
+var eddie_view;
+
 $('document').ready(function() {
 
+  eddie_view = new EddieView({"el": $('#eddie')});
+  // ice_view = new IceView({"el": $('#ice')});
+  // plaque_view = new PlaqueView({"el": $('#plaque')});
+
+  eddie_view.render();
   console.log('hello world');
 
 });
---
+++ app/view/Eddie.js
@@ -0,0 +1,10 @@
+var EddieView = Backbone.View.extend({
+
+  render: function() {
+    this.$el.html("<hr/><h3>eddie would go</h3>");
+    return this;
+  }
+
+
+});
+
