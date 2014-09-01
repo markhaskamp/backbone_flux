e6350def91f5757d60894f0397c2fbec12614aef
Mark Haskamp <mark.haskamp@pinnsg.com>
2014-09-01 12:45:21 -0400

    EddieView receiving the appload message

--- app/index.js
+++ app/index.js
@@ -2,14 +2,26 @@ var eddie_view;
 var ice_view;
 var plaque_view;
 
+var myDispatcher;
+var __DEV__ = null;
+
 $('document').ready(function() {
 
-  eddie_view = new EddieView({"el": $('#eddie')});
+  myDispatcher = new Dispatcher();
+
+  eddie_view = new EddieView({
+                              "el": $('#eddie')
+                             });
+
   ice_view = new IceView({"el": $('#ice')});
   plaque_view = new PlaqueView({"el": $('#plaque')});
 
-  eddie_view.render();
   ice_view.render();
   plaque_view.render();
+
+  myDispatcher.dispatch({
+    actionType: 'appLoad'
+  });
+
 });
 
--- app/view/Eddie.js
+++ app/view/Eddie.js
@@ -1,8 +1,16 @@
 var EddieView = Backbone.View.extend({
 
+  initialize: function() {
+    myDispatcher.register(this.foo);
+  },
+
   render: function() {
     this.$el.html("<hr/><h3>eddie would go</h3>");
     return this;
+  },
+
+  foo: function(payload) {
+    eddie_view.render();
   }
 
 
